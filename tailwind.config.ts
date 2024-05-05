de406ab96aa87789c7523abdc235a5d9edeec714
Rian Ernesto <rianernesto9@gmail.com>
2024-05-04 23:49:18 -0300

    :sparkles: feat: LanguageSwitcher

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,12 @@ const config: Config = {
       fontFamily: light.fontFamilies,
       fontSize: light.fontSizes,
       lineHeight: light.lineHeights,
+      transitionProperty: {
+        maxWidth: "max-width"
+      },
+      boxShadow: {
+        languageSwitcher: "0px 144px 40px 0px rgba(0, 0, 0, 0.01), 0px 92px 37px 0px rgba(0, 0, 0, 0.06), 0px 52px 31px 0px rgba(0, 0, 0, 0.20), 0px 23px 23px 0px rgba(0, 0, 0, 0.34), 0px 6px 13px 0px rgba(0, 0, 0, 0.39)"
+      }
     },
   },
   plugins: [],
