3b4a4015b30ae29ceaa6e8842245f50a9ca9f18c
Rian Ernesto <rianernesto9@gmail.com>
2024-05-05 00:46:20 -0300

    :lipstick: styles: Responsividade do BounceButton aplicada

--- src/components/atoms/BounceButton/index.tsx
+++ src/components/atoms/BounceButton/index.tsx
@@ -6,9 +6,9 @@ import { cn } from "@/services/utils/className.utils";
 const BounceButton = forwardRef(({className, children, ...props}: BounceButtonProps, ref: Ref<HTMLButtonElement>) => {
 
     return (
-        <button className={cn("flex flex-col items-center animate-bounce focus-amazon-hacking text-18 font-medium leading-4 rounded-xl p-2 text-white hover:text-neutrals-300 transition-colors", className)} ref={ref} {...props}>
+        <button className={cn("flex flex-col items-center animate-bounce lg:text-24 focus-amazon-hacking text-18 font-medium leading-4 rounded-xl p-2 text-white hover:text-neutrals-300 transition-colors", className)} ref={ref} {...props}>
             <span>{children}</span>
-            <RiArrowDownSFill size={28} className="animate-bounce"/>
+            <RiArrowDownSFill className="animate-bounce size-7 lg:size-10"/>
         </button>
     )
 })
