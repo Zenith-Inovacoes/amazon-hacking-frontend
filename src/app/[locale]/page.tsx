486b5a46b70e227b65e199f161f5f342104d8ad0
Rian Ernesto <rianernesto9@gmail.com>
2024-05-05 21:30:34 -0300

    :sparkles: feat: Partners

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import Partners from "@/components/templates/Partners";
 
 export default function Home() {
   const t = useTranslations("Index")
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-60 font-bold tracking-widest">
       {t("title")}
+      <Partners />
     </main>
   );
 }
