c79de58b783fc46b42ed76c7bc2440e6b7c8e1e4
Lucas Carvalho <112731358+Lucas-CarvalhoK@users.noreply.github.com>
2024-05-09 19:06:26 -0300

    :books: docs documentação storybook

--- src/components/atoms/Link/stories.tsx
+++ src/components/atoms/Link/stories.tsx
@@ -8,7 +8,12 @@ export default {
     ],
     title: "Components/Atoms/Link",
     component: Link,
-    tags: ['autodocs']
+    tags: ['autodocs'],
+    argTypes: {
+        target: {
+            description: "Default anchor property. In the case of this component, if the target is equal to `_blank`, it will also display an icon next to the url text.",
+        }
+    }
 } as Meta<LinkProps>
 
 type Story = StoryObj<LinkProps>
@@ -16,7 +21,14 @@ type Story = StoryObj<LinkProps>
 export const Default: Story = {
     args: {
         href: "https://zenithinova.com.br",
-        target: "_blank",
         children: "Explore mais o universo da Zenith"
+    }
+}
+
+export const WithTargetBlank: Story = {
+    args: {
+        href: "https://zenithinova.com.br",
+        target: "_blank",
+        children: "Lorem ipsum",
     }
 }
