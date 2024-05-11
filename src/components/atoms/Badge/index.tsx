284ea58c9ab4e94d6efec11b6eedf6e7cf1f7b74
André Sidrim <andrecorreasidrim@gmail.com>
2024-05-11 15:59:53 -0300

    :ok_hand: style: Correcao da largura do componente

--- src/components/atoms/Badge/index.tsx
+++ src/components/atoms/Badge/index.tsx
@@ -5,7 +5,7 @@ const Badge = ({ children, className, ...props }: BadgeProps) => {
   return (
     <div
       className={cn(
-        'bg-white rounded-[52px] text-black w-16 h-5 text-center items-center flex-shrink-0 flex text-xs tracking-[0.144px] justify-center font-bold',
+        'bg-white rounded-[52px] text-black w-fit h-fit text-center items-center flex-shrink-0 flex text-xs tracking-[0.144px] justify-center font-bold px-1 py-0.5',
         className
       )}
       {...props}
