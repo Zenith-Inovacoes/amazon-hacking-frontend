24978759318a2e3170b7d24a91808d6517a59f60
DanielMarcelino65 <danmarcelino6573@gmail.com>
2024-05-24 09:47:42 -0300

    :ok_hand: style Mudança no Padding do LanguageSwitcher e no Z-Index

--- src/components/atoms/LanguageSwitcher/index.tsx
+++ src/components/atoms/LanguageSwitcher/index.tsx
@@ -1,51 +1,53 @@
-'use client'
+'use client';
 
-import * as SelectPrimitive from '@radix-ui/react-select'
-import LanguageSwitcherProps from './types'
-import { forwardRef, Ref, useEffect, useState } from 'react'
-import { US, BR, ES } from 'country-flag-icons/react/3x2'
-import { IoIosArrowDown } from 'react-icons/io'
-import { usePathname, useRouter } from 'next/navigation'
-import { cn } from '@/services/utils/className.utils'
+import * as SelectPrimitive from '@radix-ui/react-select';
+import LanguageSwitcherProps from './types';
+import { forwardRef, Ref, useEffect, useState } from 'react';
+import { US, BR, ES } from 'country-flag-icons/react/3x2';
+import { IoIosArrowDown } from 'react-icons/io';
+import { usePathname, useRouter } from 'next/navigation';
+import { cn } from '@/services/utils/className.utils';
 
 const locales = [
   {
     locale: 'pt',
     label: 'Português',
-    flag: <BR title='Português' className='w-6 h-4 inline' />,
+    flag: <BR title="Português" className="w-6 h-4 inline" />,
   },
   {
     locale: 'en',
     label: 'English',
-    flag: <US title='English' className='w-6 h-4 inline' />,
+    flag: <US title="English" className="w-6 h-4 inline" />,
   },
-]
+];
 
 const LanguageSwitcher = forwardRef(
   (
     { className, ...props }: LanguageSwitcherProps,
     ref: Ref<HTMLButtonElement>
   ) => {
-    const pathname = usePathname()
-    const router = useRouter()
-    const [currentLanguage, setCurrentLanguage] = useState(locales[0])
+    const pathname = usePathname();
+    const router = useRouter();
+    const [currentLanguage, setCurrentLanguage] = useState(locales[0]);
 
     useEffect(() => {
       if (pathname) {
-        const locale = pathname.split('/')[1]
-        const language = locales.find((l) => l.locale === locale)
+        const locale = pathname.split('/')[1];
+        const language = locales.find((l) => l.locale === locale);
         if (language) {
-          setCurrentLanguage(language)
+          setCurrentLanguage(language);
         }
       }
-    }, [pathname])
+    }, [pathname]);
 
     return (
       <SelectPrimitive.Root
         value={currentLanguage.locale}
         onValueChange={(value) => {
-          router.push(`/${value}`)
-          setCurrentLanguage(locales.find((locale) => locale.locale === value)!)
+          router.push(`/${value}`);
+          setCurrentLanguage(
+            locales.find((locale) => locale.locale === value)!
+          );
         }}
         {...props}
       >
@@ -57,14 +59,14 @@ const LanguageSwitcher = forwardRef(
           )}
         >
           <SelectPrimitive.Value>
-            <div className='flex items-center whitespace-nowrap'>
+            <div className="flex items-center whitespace-nowrap">
               {currentLanguage.flag}
-              <span className='ml-2 opacity-0 group-data-[state=open]:visible group-hover:opacity-100 group-data-[state=open]:opacity-100 transition-all duration-500'>
+              <span className="ml-2 opacity-0 group-data-[state=open]:visible group-hover:opacity-100 group-data-[state=open]:opacity-100 transition-all duration-500">
                 {currentLanguage.label}
               </span>
               <SelectPrimitive.Icon
                 asChild
-                className='ml-2 group-data-[state=open]:visible opacity-0 group-hover:opacity-100 group-data-[state=open]:opacity-100 group-data-[state=open]:rotate-180 group-data-[state=closed]:rotate-0 transition-all duration-500'
+                className="ml-2 group-data-[state=open]:visible opacity-0 group-hover:opacity-100 group-data-[state=open]:opacity-100 group-data-[state=open]:rotate-180 group-data-[state=closed]:rotate-0 transition-all duration-500"
               >
                 <IoIosArrowDown size={24} />
               </SelectPrimitive.Icon>
@@ -73,19 +75,19 @@ const LanguageSwitcher = forwardRef(
         </SelectPrimitive.Trigger>
         <SelectPrimitive.Portal>
           <SelectPrimitive.Content
-            className='min-w-[var(--radix-select-trigger-width)] shadow-languageSwitcher cursor-pointer'
-            position='popper'
+            className="min-w-[var(--radix-select-trigger-width)]  z-[9999]  shadow-languageSwitcher cursor-pointer"
+            position="popper"
           >
             <SelectPrimitive.Viewport>
               {locales.map((locale, index) => (
                 <SelectPrimitive.Item
                   key={index}
                   value={locale.locale}
-                  className='font-normal text-base leading-4 px-3 py-2 bg-neutrals-600 focus-visible:outline-none hover:bg-neutrals-700 focus-visible:bg-neutrals-700 transition-colors data-[state=checked]:hidden'
+                  className="font-normal text-base leading-4 px-4 py-4 bg-neutrals-600 focus-visible:outline-none hover:bg-neutrals-700 focus-visible:bg-neutrals-700 transition-colors data-[state=checked]:hidden"
                 >
-                  <SelectPrimitive.ItemText className='flex items-center'>
+                  <SelectPrimitive.ItemText className="flex items-center">
                     {locale.flag}
-                    <span className='ml-2'>{locale.label}</span>
+                    <span className="ml-2">{locale.label}</span>
                   </SelectPrimitive.ItemText>
                 </SelectPrimitive.Item>
               ))}
@@ -93,10 +95,10 @@ const LanguageSwitcher = forwardRef(
           </SelectPrimitive.Content>
         </SelectPrimitive.Portal>
       </SelectPrimitive.Root>
-    )
+    );
   }
-)
+);
 
-LanguageSwitcher.displayName = 'Language Switcher'
+LanguageSwitcher.displayName = 'Language Switcher';
 
-export default LanguageSwitcher
+export default LanguageSwitcher;
