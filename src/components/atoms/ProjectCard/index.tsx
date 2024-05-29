c780509265a1a23770390c7927ecd466b1e51d30
Lucas Carvalho <lucas_carvalhok@outlook.com>
2024-05-29 13:11:20 -0300

    :sparkles: tipagem do ProjectCard

--- src/components/atoms/ProjectCard/index.tsx
+++ src/components/atoms/ProjectCard/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import ProjectCardProps from './types'
 import React from 'react';
 import { cn } from '@/services/utils/className.utils'
@@ -12,8 +14,8 @@ import Image from 'next/image';
 export default function ProjectCard({ ...props }: ProjectCardProps) {
   const pathname = usePathname()
   const [locale, setLocale] = useState<string>(
-      locales.find((locale) => pathname?.includes(locale.locale))?.locale ||
-          'en'
+    locales.find((locale) => pathname?.includes(locale.locale))?.locale ||
+    'en'
   )
 
   const ytURL = locale === 'en' ? props.links.youtube.en : props.links.youtube.pt
@@ -28,13 +30,15 @@ export default function ProjectCard({ ...props }: ProjectCardProps) {
   }
 
   return (
-    <div className='bg-white flex'>
-      <div className='w-[87px] h-[87px] rounded-[50%]'>
-        <Image src={props.logoUrl || LogoComputacaoAmostra} alt={`Logo da equipe ${props.name}`} fill />
+    <div className='bg-white flex w-[300px] p-6 rounded-xl justify-center gap-x-8 hover:p-8 transition-all duration-500'>
+      <div className='w-[87px] h-[87px] flex justify-center items-center rounded-[50%] bg-black'>
+        <div className='relative w-14 h-14'>
+          <Image src={props.logoUrl || LogoComputacaoAmostra} alt={`Logo da equipe ${props.name}`} fill />
+        </div>
       </div>
-      <div>
-        <span>{props.name}</span>
-        <ProjectOverlay variant='current' videoURL={ytURL} overlayTrigger={<Button>Conheça mais...</Button>}>
+      <div className='flex flex-col gap-y-6 items-center'>
+        <span className='text-black text-16 font-bold'>{props.name}</span>
+        <ProjectOverlay variant='current' videoURL={ytURL} overlayTrigger={<Button variant='primary' className='rounded-3xl px-3 py-2'>Conheça mais...</Button>}>
           {childrenData}
         </ProjectOverlay>
       </div>
--- src/components/atoms/ProjectCard/stories.tsx
+++ src/components/atoms/ProjectCard/stories.tsx
@@ -1,69 +1,59 @@
 import { Meta, StoryObj } from "@storybook/react"
 import ProjectCard from "."
-import ProjectCardProps, { ProjectOverlayChildrenProps }  from "./types"
-
-import Button from '@/components/atoms/Button'
+import { ProjectCardProps } from "./types"
 import ZenithLogo from 'public/ZenithLogoBranca.svg'
-import { CourseEnum } from "@/types/IProject"
-import { string, boolean, number } from "zod"
 
-const childrenData: ProjectOverlayChildrenProps = {
-    solutionName: 'NOME DA SOLUCAO',
-    teamLogo: ZenithLogo,
-    solutionDescription:
-      'Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem.\n\nMorbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus.',
-    altLogo: 'Logo da Zenith Inova',
-    teamMembers: [
-      'Gabriel Marcelino',
-      'Rian Ernesto',
-      'Marco Antonio',
-      'Elissandra Nascimento',
-      'Daniel Marcelino',
-      'Fernanda Panzera',
-      'André Sidrim',
-      'Lucas Gabriel',
-    ],
-  }
+import { CourseEnum } from "@/types/IProject"
+
+// Dados de teste
+const teste = {
+  id: "123456",
+  name: "Project Alpha",
+  team: ["Alice Smith", "Bob Johnson", "Charlie Davis"],
+  description: "A comprehensive project aimed at developing an innovative solution for sustainable energy.",
+  status: true,
+  links: {
+    github: "https://github.com/example/project-alpha",
+    youtube: {
+      en: "https://www.youtube.com/watch?v=bJhPNFamNwk",
+      pt: "https://www.youtube.com/watch?v=bJhPNFamNwk"
+    },
+    banner: "https://example.com/banner.png"
+  },
+  course: CourseEnum.ECOMP,
+  year: 2024
+}
 
 export default {
-    decorators: [
-        (Story) => <div className="w-full py-8 grid place-items-center"><Story /></div>
-    ],
-    title: "Components/Atoms/ProjectCard",
-    component: ProjectCard,
-    tags: ['autodocs'],
-    argTypes: {
-        target: {
-            description: "Default anchor property. In the case of this component, if the target is equal to `_blank`, it will also display an icon next to the url text.",
-        }
+  decorators: [
+    (Story) => <div className="w-full py-8 grid place-items-center"><Story /></div>
+  ],
+  title: "Components/Atoms/ProjectCard",
+  component: ProjectCard,
+  tags: ['autodocs'],
+  argTypes: {
+    target: {
+      description: "Default anchor property. In the case of this component, if the target is equal to `_blank`, it will also display an icon next to the url text.",
     }
+  }
 } as Meta<ProjectCardProps>
 
 type Story = StoryObj<ProjectCardProps>
 
 export const Success: Story = {    
-    render: () => {
-        return (
-          <ProjectCard
-            id="1"
-            name="Tupã"
-            am: string[],
-    description: string,
-    status?: boolean
-    links: {
-        github: string,
-        youtube: {
-            en: string,
-            pt: string
-        },
-        banner?: string
-    },
-    course: CourseEnum,
-    logoUrl?: string,
-    year?: number
-       
-          >
-          </ProjectCard>
-        )
-      },
-    }
+  render: () => {
+    return (
+      <ProjectCard
+        id={teste.id}
+        name={teste.name}
+        team={teste.team}
+        description={teste.description}
+        status={teste.status}
+        links={teste.links}
+        course={teste.course}
+      
+        year={teste.year}
+      />
+    )
+  },
+}
