d338b964723cee006ed132bb10d5801224da30c2
Lucas Carvalho <112731358+Lucas-CarvalhoK@users.noreply.github.com>
2024-06-03 14:46:04 -0300

    :sparkles: feat finalização da seção ODSs

--- src/components/templates/ODSs/components/index.tsx
+++ src/components/templates/ODSs/components/index.tsx
@@ -4,7 +4,7 @@ import { ODSBoxProps } from './types'
 
 export default function ODSBox({ image, url, odsName, classname, ...props }: ODSBoxProps) {
     return (
-        <a href={url} target='_blank' className={cn('relative inline-block w-[92px] h-[92px] lg:w-[126px] lg:h-[126px] hover:scale-105 duration-200 transition-all', classname)}>
+        <a href={url} target='_blank' className={cn('relative inline-block w-full max-w-[92px] h-[92px] lg:w-[126px] lg:h-[126px] hover:scale-105 duration-200 transition-all', classname)}>
             <Image src={image} alt={`Símbolo da ODS ${odsName}`} fill />
         </a>
     )
--- src/components/templates/ODSs/index.tsx
+++ src/components/templates/ODSs/index.tsx
@@ -23,16 +23,16 @@ export default function ODSs() {
     return (
         <section className='relative flex bg-white min-h-screen w-full flex-col items-center justify-center py-4'>
             <div className='w-full '>
-                <div className='flex flex-col gap-y-6 w-full justify-center items-center px-4 xs:px-8 md:mx-auto max-w-screen-2xl'>
+                <div className='flex flex-col gap-y-6 w-full justify-center items-center px-4 xs:px-8 md:mx-auto md:max-w-[870px] max-w-screen-2xl'>
                     <h2 className='text-36 xs:text-40 md:text-50 lg:text-60 font-bold text-black text-center'>Explore soluções inovadoras</h2>
-                    <p className='text-16 md:text-18 lg:text-20 text-neutral-500 text-center'>Durante a jornada, equipes multidisciplinares da 3ª Edição do Amazon Hacking desenvolveram projetos que abordam problemas reais e oferecem soluções práticas com impacto positivo.
+                    <p className='text-16 md:text-18 lg:text-20 text-black text-center'>Durante a jornada, equipes multidisciplinares da 3ª Edição do Amazon Hacking desenvolveram projetos que abordam problemas reais e oferecem soluções práticas com impacto positivo.
 
                     </p>
                 </div>
                 <div className="w-[100vw] h-24 opacity-60 z-0 bg-[url('/images/symbol.png')] bg-repeat bg-contain my-8" />
                 <div className='w-full flex justify-center items-center flex-col gap-y-7 mx-auto relative px-4 xs:px-8 max-w-screen-2xl'>
                     <p className='text-20 md:text-24 lg:text-28 font-bold text-center text-black'>Objetivos de Desenvolvimento Sustentável</p>
-                    <div className='relative z-10 mx-auto max-w-[90vw]'>
+                    <div className='relative z-10 max-w-[90vw] w-full md:max-w-[840px]'>
                         <MotionDiv
                             viewport={{
                                 once: true,
@@ -41,7 +41,7 @@ export default function ODSs() {
                             initial='hidden'
                             whileInView='show'
                             variants={goalCardVariants}
-                            className='horizontal-snap cursor-grab flex gap-5 max-w-screen-2xl xs:gap-x-9 py-12 md:grid md:grid-cols-6 md:items-center md:justify-center md:pl-0 relative z-10 w-full lg:mx-auto lg:place-items-center lg:cursor-default'
+                            className='relative horizontal-snap cursor-grab flex gap-5 max-w-screen-2xl xs:gap-x-9 py-12 md:flex-wrap md:pl-0 md:justify-center md:items-center z-10 w-full md:cursor-default md:py-0'
                         >
                             {ODSsData.map(({ image, url, odsName }, index) => (
                                 <ODSBox
