0836a40af68a257ecb574c627ccaa9a7e2e683eb
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-06-04 11:38:20 -0300

    feat: Add Avatar component for displaying user avatars
    This commit adds the Avatar component to the atoms directory. The Avatar component is a reusable component that renders a circular avatar with an optional fallback and image. It improves the code organization and provides a consistent way to display user avatars throughout the application.

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,15 +1,33 @@
-import BounceButton from './BounceButton'
-import Button from './Button'
-import FloatingButton from './FloatingButton'
-import FooterButton from './FooterButton'
-import SocialMediaButton from './SocialMediaButton'
-import Badge from './Badge'
-import Checkbox from './Checkbox'
-import LanguageSwitcher from './LanguageSwitcher'
-import Link from './Link'
-import LogoButton from './LogoButton'
-import { Overlay, OverlayContent, OverlayTrigger } from './Overlay'
-import Select from './Select'
+import BounceButton from "./BounceButton";
+import Button from "./Button";
+import FloatingButton from "./FloatingButton";
+import FooterButton from "./FooterButton";
+import SocialMediaButton from "./SocialMediaButton";
+import Badge from "./Badge";
+import Checkbox from "./Checkbox";
+import LanguageSwitcher from "./LanguageSwitcher";
+import Link from "./Link";
+import LogoButton from "./LogoButton";
+import { Overlay, OverlayContent, OverlayTrigger } from "./Overlay";
+import Select from "./Select";
+import { Avatar, AvatarFallback, AvatarImage } from "./Avatar";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./DropdownMenu";
 
 export {
   BounceButton,
@@ -26,4 +44,22 @@ export {
   OverlayContent,
   OverlayTrigger,
   Select,
-}
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+};
