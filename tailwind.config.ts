84d94a43481583b5cc999a610b2da2b550de576b
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-06-04 11:34:43 -0300

    refactor: Update animation timings for accordion
    This commit updates the animation timings for the accordion component in the `tailwind.config.ts` file. It adds two new animation classes, "accordion-down" and "accordion-up", with durations of 0.2s and easing of ease-out. These changes improve the user experience by providing smoother and more responsive accordion animations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -136,6 +136,14 @@ const config: Config = {
                         marginRight: '230px',
                     },
                 },
+                "accordion-down": {
+                    from: { height: '0' },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: '0' },
+                },
             },
             animation: {
                 overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
@@ -153,9 +161,11 @@ const config: Config = {
                 iconOpenXL: 'iconOpenXL 300ms ease-in-out',
                 iconCloseXL: 'iconCloseXL 300ms ease-in-out',
                 desktopMenuOverlayHide: 'overlayHide 300ms ease-in-out 600ms',
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 }
 export default config
