2bd95611eaf92d39561ce73dd9d7431219c35325
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-06-04 11:37:05 -0300

    feat: Add layout and page components for solutions
    This commit adds the layout and page components for the solutions feature. The layout component, `Layout`, includes the `Header` component and renders its children. The page component, `SolutionsPage`, renders the `Solutions` and `Tucupy` components. These changes improve the code organization and provide a consistent layout for the solutions pages.

---
+++ src/app/[locale]/solutions/page.tsx
@@ -0,0 +1,11 @@
+import Solutions from "@/components/templates/Solutions";
+import Tucupy from "@/components/templates/Tucupy";
+
+export default function SolutionsPage() {
+  return (
+    <>
+      <Solutions />
+      <Tucupy />
+    </>
+  );
+}
