78637d60ad296876e871dcfb5be6c753b7a99b6b
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-06-04 11:40:07 -0300

    refactor: Update SocialMediaButton component with target and rel attributes
    This commit updates the SocialMediaButton component in the atoms directory. It adds the `target="_blank"` and `rel="noreferrer noopener"` attributes to the anchor element. This change ensures that the social media links open in a new tab and improves the accessibility and security of the application.

--- src/components/atoms/SocialMediaButton/index.tsx
+++ src/components/atoms/SocialMediaButton/index.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/services/utils/className.utils"
 
 const SocialMediaButton = forwardRef(({ className, children, variant = 'outline', ...props }: SocialMediaButtonProps, ref: Ref<HTMLAnchorElement>) => {
     return (
-        <a className={cn("h-10 w-10 flex items-center justify-center relative hover:scale-110 active:opacity-80 duration-300 transition-transform focus-visible:outline-none focus-visible:outline-1 focus-visible:outline-offset-8 focus-visible:outline-primary-100", variant === 'fill' ? "bg-white rounded-full" : "rounded-lg", className)} ref={ref} {...props}>
+        <a className={cn("h-10 w-10 flex items-center justify-center relative hover:scale-110 active:opacity-80 duration-300 transition-transform focus-visible:outline-none focus-visible:outline-1 focus-visible:outline-offset-8 focus-visible:outline-primary-100", variant === 'fill' ? "bg-white rounded-full" : "rounded-lg", className)} ref={ref} {...props} target="_blank" rel="noreferrer noopener">
             <div className={cn("flex justify-center items-center", variant === 'fill' && "h-6 w-6")}>
                 {children}
             </div>
